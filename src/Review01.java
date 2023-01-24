13e5c3659de072fca529646a7ab34359704fd1d6
nakamuratakeru <nakamuratakeru@localhost>
2023-01-24 21:43:24 +0900

    修正しました。

--- src/Review01.java
+++ src/Review01.java
@@ -3,13 +3,17 @@ public class Review01 {
 
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
-        add(1500, 10);
+        int num1 = 1500;
+        int num2 = 10;
+        double result = tax(num1, num2);
+        int sum = num1 + (int)result;
+
+        System.out.println(num1 + "の商品の税込価格は" + sum + "円（消費税は" + (int)result + "円）です。");
 
     }
-    public static void add(int a, int b) {
-        int tax = a / b;
-        int num = a + tax;
-        System.out.println(a + "円の商品の税込価格は" + num + "円（消費税は" + tax +"円）です。");
+    public static double tax(int num1, int num2) {
+         double tax = num1 * num2 / 100;
+        return tax;
     }
 
 }
